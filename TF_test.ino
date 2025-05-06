8ec8fb8b8ab5e7ce1b1a168b338004daa5bfa6de
yhe21 <h375028969@gmail.com>
2025-05-06 09:33:24 -0400

    TF test initial commit

---
+++ TF_test.ino
@@ -0,0 +1,467 @@
+/**********************************************************
+*** Emm_V5.0步进闭环控制例程
+*** 编写作者：ZHANGDATOU
+*** 技术支持：张大头闭环伺服
+*** 淘宝店铺：https://zhangdatou.taobao.com
+*** CSDN博客：http s://blog.csdn.net/zhangdatou666
+*** qq交流群：262438510
+**********************************************************/
+
+// 注意：串口和USB下载口共用串口（0,1），USB上传程序时，先拔掉串口线.
+
+#define    ABS(x)    ((x) > 0 ? (x) : -(x)) 
+
+typedef enum {
+  S_VER   = 0,      /* 读取固件版本和对应的硬件版本 */
+  S_RL    = 1,      /* 读取读取相电阻和相电感 */
+  S_PID   = 2,      /* 读取PID参数 */
+  S_VBUS  = 3,      /* 读取总线电压 */
+  S_CPHA  = 5,      /* 读取相电流 */
+  S_ENCL  = 7,      /* 读取经过线性化校准后的编码器值 */
+  S_TPOS  = 8,      /* 读取电机目标位置角度 */
+  S_VEL   = 9,      /* 读取电机实时转速 */
+  S_CPOS  = 10,     /* 读取电机实时位置角度 */
+  S_PERR  = 11,     /* 读取电机位置误差角度 */
+  S_FLAG  = 13,     /* 读取使能/到位/堵转状态标志位 */
+  S_Conf  = 14,     /* 读取驱动参数 */
+  S_State = 15,     /* 读取系统状态参数 */
+  S_ORG   = 16,     /* 读取正在回零/回零失败状态标志位 */
+}SysParams_t;
+
+/**********************************************************
+*** 注意：每个函数的参数的具体说明，请查阅下方的函数的注释说明
+**********************************************************/
+void Emm_V5_Reset_CurPos_To_Zero(uint8_t addr); // 将当前位置清零
+void Emm_V5_Reset_Clog_Pro(uint8_t addr); // 解除堵转保护
+void Emm_V5_Read_Sys_Params(uint8_t addr, SysParams_t s); // 读取参数
+void Emm_V5_Modify_Ctrl_Mode(uint8_t addr, bool svF, uint8_t ctrl_mode); // 发送命令修改开环/闭环控制模式
+void Emm_V5_En_Control(uint8_t addr, bool state, bool snF); // 电机使能控制
+void Emm_V5_Vel_Control(uint8_t addr, uint8_t dir, uint16_t vel, uint8_t acc, bool snF); // 速度模式控制
+void Emm_V5_Pos_Control(uint8_t addr, uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF); // 位置模式控制
+void Emm_V5_Stop_Now(uint8_t addr, bool snF); // 让电机立即停止运动
+void Emm_V5_Synchronous_motion(uint8_t addr); // 触发多机同步开始运动
+void Emm_V5_Origin_Set_O(uint8_t addr, bool svF); // 设置单圈回零的零点位置
+void Emm_V5_Origin_Modify_Params(uint8_t addr, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF); // 修改回零参数
+void Emm_V5_Origin_Trigger_Return(uint8_t addr, uint8_t o_mode, bool snF); // 发送命令触发回零
+void Emm_V5_Origin_Interrupt(uint8_t addr); // 强制中断并退出回零
+void Emm_V5_Receive_Data(uint8_t *rxCmd, uint8_t *rxCount); // 返回数据接收函数
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // 初始化LED灯
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // 初始化串口
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  // 等待串口初始化完成
+  //while (!Serial) {
+  //  ; // wait for serial port to connect. Needed for native USB port only
+  //}
+
+  // 上电延时2秒等待Emm_V5.0闭环初始化完毕
+  delay(2000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // 定义接收数据数组、接收数据长度
+  uint8_t rxCmd[128] = {0}; uint8_t rxCount = 0;
+ 
+  // 位置模式：速度1000RPM，加速度0（不使用加减速直接启动），脉冲数3200（16细分下发送3200个脉冲电机转一圈），相对运动
+  Emm_V5_Pos_Control(1, 0, 300, 210, 15000, 1, 0);
+  delay(1500);
+  Emm_V5_Pos_Control(1, 0, 300, 210, 25000, 1, 0);
+  delay(1500);
+  Emm_V5_Pos_Control(1, 0, 300, 210, 35000, 1, 0);
+  delay(1500);
+  
+  // 等待返回命令，命令数据缓存在数组rxCmd上，长度为rxCount
+  //Emm_V5_Receive_Data(rxCmd, &rxCount);
+
+  // 验证校验字节，验证成功则点亮LED灯，否则熄灭LED灯
+  //if(rxCmd[rxCount - 1] == 0x6B) { digitalWrite(LED_BUILTIN, LOW); } else { digitalWrite(LED_BUILTIN, HIGH); }
+  
+  // 调试使用，打印Emm_V5.0闭环返回的数据到串口
+  //for(int i = 0; i < rxCount; i++) { Serial.print(rxCmd[i],HEX); } // 因为和USB下载口共用串口，所以让每个数据加1再发送出来，防止和电机地址冲突
+//Serial.print(12,HEX);
+  // 停止发送命令
+  //
+  
+  Emm_V5_Pos_Control(1, 0, 300, 210, 0, 1, 0);
+  delay(6000);
+}
+
+/**
+  * @brief    将当前位置清零
+  * @param    addr  ：电机地址
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Reset_CurPos_To_Zero(uint8_t addr)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x0A;                       // 功能码
+  cmd[2] =  0x6D;                       // 辅助码
+  cmd[3] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 4);
+}
+
+/**
+  * @brief    解除堵转保护
+  * @param    addr  ：电机地址
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Reset_Clog_Pro(uint8_t addr)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x0E;                       // 功能码
+  cmd[2] =  0x52;                       // 辅助码
+  cmd[3] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 4);
+}
+
+/**
+  * @brief    读取系统参数
+  * @param    addr  ：电机地址
+  * @param    s     ：系统参数类型
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Read_Sys_Params(uint8_t addr, SysParams_t s)
+{
+  uint8_t i = 0;
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[i] = addr; ++i;                   // 地址
+
+  switch(s)                             // 功能码
+  {
+    case S_VER  : cmd[i] = 0x1F; ++i; break;
+    case S_RL   : cmd[i] = 0x20; ++i; break;
+    case S_PID  : cmd[i] = 0x21; ++i; break;
+    case S_VBUS : cmd[i] = 0x24; ++i; break;
+    case S_CPHA : cmd[i] = 0x27; ++i; break;
+    case S_ENCL : cmd[i] = 0x31; ++i; break;
+    case S_TPOS : cmd[i] = 0x33; ++i; break;
+    case S_VEL  : cmd[i] = 0x35; ++i; break;
+    case S_CPOS : cmd[i] = 0x36; ++i; break;
+    case S_PERR : cmd[i] = 0x37; ++i; break;
+    case S_FLAG : cmd[i] = 0x3A; ++i; break;
+    case S_ORG  : cmd[i] = 0x3B; ++i; break;
+    case S_Conf : cmd[i] = 0x42; ++i; cmd[i] = 0x6C; ++i; break;
+    case S_State: cmd[i] = 0x43; ++i; cmd[i] = 0x7A; ++i; break;
+    default: break;
+  }
+
+  cmd[i] = 0x6B; ++i;                   // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, i);
+}
+
+/**
+  * @brief    修改开环/闭环控制模式
+  * @param    addr     ：电机地址
+  * @param    svF      ：是否存储标志，false为不存储，true为存储
+  * @param    ctrl_mode：控制模式（对应屏幕上的P_Pul菜单），0是关闭脉冲输入引脚，1是开环模式，2是闭环模式，3是让En端口复用为多圈限位开关输入引脚，Dir端口复用为到位输出高电平功能
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Modify_Ctrl_Mode(uint8_t addr, bool svF, uint8_t ctrl_mode)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x46;                       // 功能码
+  cmd[2] =  0x69;                       // 辅助码
+  cmd[3] =  svF;                        // 是否存储标志，false为不存储，true为存储
+  cmd[4] =  ctrl_mode;                  // 控制模式（对应屏幕上的P_Pul菜单），0是关闭脉冲输入引脚，1是开环模式，2是闭环模式，3是让En端口复用为多圈限位开关输入引脚，Dir端口复用为到位输出高电平功能
+  cmd[5] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 6);
+}
+
+/**
+  * @brief    使能信号控制
+  * @param    addr  ：电机地址
+  * @param    state ：使能状态     ，true为使能电机，false为关闭电机
+  * @param    snF   ：多机同步标志 ，false为不启用，true为启用
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_En_Control(uint8_t addr, bool state, bool snF)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0xF3;                       // 功能码
+  cmd[2] =  0xAB;                       // 辅助码
+  cmd[3] =  (uint8_t)state;             // 使能状态
+  cmd[4] =  snF;                        // 多机同步运动标志
+  cmd[5] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 6);
+}
+
+/**
+  * @brief    速度模式
+  * @param    addr：电机地址
+  * @param    dir ：方向       ，0为CW，其余值为CCW
+  * @param    vel ：速度       ，范围0 - 5000RPM
+  * @param    acc ：加速度     ，范围0 - 255，注意：0是直接启动
+  * @param    snF ：多机同步标志，false为不启用，true为启用
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Vel_Control(uint8_t addr, uint8_t dir, uint16_t vel, uint8_t acc, bool snF)
+{
+  uint8_t cmd[16] = {0};
+
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0xF6;                       // 功能码
+  cmd[2] =  dir;                        // 方向
+  cmd[3] =  (uint8_t)(vel >> 8);        // 速度(RPM)高8位字节
+  cmd[4] =  (uint8_t)(vel >> 0);        // 速度(RPM)低8位字节
+  cmd[5] =  acc;                        // 加速度，注意：0是直接启动
+  cmd[6] =  snF;                        // 多机同步运动标志
+  cmd[7] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 8);
+}
+
+/**
+  * @brief    位置模式
+  * @param    addr：电机地址
+  * @param    dir ：方向        ，0为CW，其余值为CCW
+  * @param    vel ：速度(RPM)   ，范围0 - 5000RPM
+  * @param    acc ：加速度      ，范围0 - 255，注意：0是直接启动
+  * @param    clk ：脉冲数      ，范围0- (2^32 - 1)个
+  * @param    raF ：相位/绝对标志，false为相对运动，true为绝对值运动
+  * @param    snF ：多机同步标志 ，false为不启用，true为启用
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Pos_Control(uint8_t addr, uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF)
+{
+  uint8_t cmd[16] = {0};
+
+  // 装载命令
+  cmd[0]  =  addr;                      // 地址
+  cmd[1]  =  0xFD;                      // 功能码
+  cmd[2]  =  dir;                       // 方向
+  cmd[3]  =  (uint8_t)(vel >> 8);       // 速度(RPM)高8位字节
+  cmd[4]  =  (uint8_t)(vel >> 0);       // 速度(RPM)低8位字节 
+  cmd[5]  =  acc;                       // 加速度，注意：0是直接启动
+  cmd[6]  =  (uint8_t)(clk >> 24);      // 脉冲数(bit24 - bit31)
+  cmd[7]  =  (uint8_t)(clk >> 16);      // 脉冲数(bit16 - bit23)
+  cmd[8]  =  (uint8_t)(clk >> 8);       // 脉冲数(bit8  - bit15)
+  cmd[9]  =  (uint8_t)(clk >> 0);       // 脉冲数(bit0  - bit7 )
+  cmd[10] =  raF;                       // 相位/绝对标志，false为相对运动，true为绝对值运动
+  cmd[11] =  snF;                       // 多机同步运动标志，false为不启用，true为启用
+  cmd[12] =  0x6B;                      // 校验字节
+  
+  // 发送命令
+  Serial1.write(cmd, 13);
+
+}
+
+/**
+  * @brief    立即停止（所有控制模式都通用）
+  * @param    addr  ：电机地址
+  * @param    snF   ：多机同步标志，false为不启用，true为启用
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Stop_Now(uint8_t addr, bool snF)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0xFE;                       // 功能码
+  cmd[2] =  0x98;                       // 辅助码
+  cmd[3] =  snF;                        // 多机同步运动标志
+  cmd[4] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 5);
+}
+
+/**
+  * @brief    多机同步运动
+  * @param    addr  ：电机地址
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Synchronous_motion(uint8_t addr)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0xFF;                       // 功能码
+  cmd[2] =  0x66;                       // 辅助码
+  cmd[3] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 4);
+}
+
+/**
+  * @brief    设置单圈回零的零点位置
+  * @param    addr  ：电机地址
+  * @param    svF   ：是否存储标志，false为不存储，true为存储
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Origin_Set_O(uint8_t addr, bool svF)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x93;                       // 功能码
+  cmd[2] =  0x88;                       // 辅助码
+  cmd[3] =  svF;                        // 是否存储标志，false为不存储，true为存储
+  cmd[4] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 5);
+}
+
+/**
+  * @brief    修改回零参数
+  * @param    addr  ：电机地址
+  * @param    svF   ：是否存储标志，false为不存储，true为存储
+  * @param    o_mode ：回零模式，0为单圈就近回零，1为单圈方向回零，2为多圈无限位碰撞回零，3为多圈有限位开关回零
+  * @param    o_dir  ：回零方向，0为CW，其余值为CCW
+  * @param    o_vel  ：回零速度，单位：RPM（转/分钟）
+  * @param    o_tm   ：回零超时时间，单位：毫秒
+  * @param    sl_vel ：无限位碰撞回零检测转速，单位：RPM（转/分钟）
+  * @param    sl_ma  ：无限位碰撞回零检测电流，单位：Ma（毫安）
+  * @param    sl_ms  ：无限位碰撞回零检测时间，单位：Ms（毫秒）
+  * @param    potF   ：上电自动触发回零，false为不使能，true为使能
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Origin_Modify_Params(uint8_t addr, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF)
+{
+  uint8_t cmd[32] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x4C;                       // 功能码
+  cmd[2] =  0xAE;                       // 辅助码
+  cmd[3] =  svF;                        // 是否存储标志，false为不存储，true为存储
+  cmd[4] =  o_mode;                     // 回零模式，0为单圈就近回零，1为单圈方向回零，2为多圈无限位碰撞回零，3为多圈有限位开关回零
+  cmd[5] =  o_dir;                      // 回零方向
+  cmd[6]  =  (uint8_t)(o_vel >> 8);     // 回零速度(RPM)高8位字节
+  cmd[7]  =  (uint8_t)(o_vel >> 0);     // 回零速度(RPM)低8位字节 
+  cmd[8]  =  (uint8_t)(o_tm >> 24);     // 回零超时时间(bit24 - bit31)
+  cmd[9]  =  (uint8_t)(o_tm >> 16);     // 回零超时时间(bit16 - bit23)
+  cmd[10] =  (uint8_t)(o_tm >> 8);      // 回零超时时间(bit8  - bit15)
+  cmd[11] =  (uint8_t)(o_tm >> 0);      // 回零超时时间(bit0  - bit7 )
+  cmd[12] =  (uint8_t)(sl_vel >> 8);    // 无限位碰撞回零检测转速(RPM)高8位字节
+  cmd[13] =  (uint8_t)(sl_vel >> 0);    // 无限位碰撞回零检测转速(RPM)低8位字节 
+  cmd[14] =  (uint8_t)(sl_ma >> 8);     // 无限位碰撞回零检测电流(Ma)高8位字节
+  cmd[15] =  (uint8_t)(sl_ma >> 0);     // 无限位碰撞回零检测电流(Ma)低8位字节 
+  cmd[16] =  (uint8_t)(sl_ms >> 8);     // 无限位碰撞回零检测时间(Ms)高8位字节
+  cmd[17] =  (uint8_t)(sl_ms >> 0);     // 无限位碰撞回零检测时间(Ms)低8位字节
+  cmd[18] =  potF;                      // 上电自动触发回零，false为不使能，true为使能
+  cmd[19] =  0x6B;                      // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 20);
+}
+
+/**
+  * @brief    触发回零
+  * @param    addr   ：电机地址
+  * @param    o_mode ：回零模式，0为单圈就近回零，1为单圈方向回零，2为多圈无限位碰撞回零，3为多圈有限位开关回零
+  * @param    snF   ：多机同步标志，false为不启用，true为启用
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Origin_Trigger_Return(uint8_t addr, uint8_t o_mode, bool snF)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x9A;                       // 功能码
+  cmd[2] =  o_mode;                     // 回零模式，0为单圈就近回零，1为单圈方向回零，2为多圈无限位碰撞回零，3为多圈有限位开关回零
+  cmd[3] =  snF;                        // 多机同步运动标志，false为不启用，true为启用
+  cmd[4] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 5);
+}
+
+/**
+  * @brief    强制中断并退出回零
+  * @param    addr  ：电机地址
+  * @retval   地址 + 功能码 + 命令状态 + 校验字节
+  */
+void Emm_V5_Origin_Interrupt(uint8_t addr)
+{
+  uint8_t cmd[16] = {0};
+  
+  // 装载命令
+  cmd[0] =  addr;                       // 地址
+  cmd[1] =  0x9C;                       // 功能码
+  cmd[2] =  0x48;                       // 辅助码
+  cmd[3] =  0x6B;                       // 校验字节
+  
+  // 发送命令
+  Serial.write(cmd, 4);
+}
+
+/**
+  * @brief    接收数据
+  * @param    rxCmd   : 接收到的数据缓存在该数组
+  * @param    rxCount : 接收到的数据长度
+  * @retval   无
+  */
+void Emm_V5_Receive_Data(uint8_t *rxCmd, uint8_t *rxCount)
+{
+  int i = 0;
+  unsigned long lTime;                    // 上一时刻的时间
+  unsigned long cTime;                    // 当前时刻的时间
+
+  // 记录当前的时间
+  lTime = cTime = millis();
+
+  // 开始接收数据
+  while(1)
+  {
+    if(Serial1.available() > 0)            // 串口有数据进来
+    {
+      if(i <= 128)                        // 防止数组溢出，该值需要小于数组的长度
+      {
+        rxCmd[i++] = Serial1.read();       // 接收数据
+
+        lTime = millis();                 // 更新上一时刻的时间
+      }
+    }
+    else                                  // 串口有没有数据
+    {
+      cTime = millis();                   // 获取当前时刻的时间
+
+      if((int)(cTime - lTime) > 100)      // 100毫秒内串口没有数据进来，就判定一帧数据接收结束
+      {
+        *rxCount = i;                     // 数据长度
+        
+        break;                            // 退出while(1)循环
+      }
+    }
+  }
+}
