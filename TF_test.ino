285f81e864922088c0c44ac68e2d0e80a047fcbb
yhe21 <h375028969@gmail.com>
2026-09-14 19:53:34 -0400

    Fix part tray ST3 acknowledgement retry state

--- TF_test.ino
+++ TF_test.ino
@@ -317,7 +317,7 @@ public:
         if (checkAck(id, st3Target)) {
           state = ST3_ACKED;
         } else {
-          state = ST2_UNSENT;
+          state = ST3_UNSENT;
         }
 
         break;
